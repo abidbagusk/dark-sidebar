94b5bae41fe3ce662860a68ac423f272bfb84d41
abidbagusk <abidbagudkurniawan@gmail.com>
2019-02-01 19:44:22 +0700

    add animation on social media

--- js/main.js
+++ js/main.js
@@ -3,4 +3,7 @@ $(document).ready(function () {
         $('.sidenav').toggleClass('open');
         $('.menu>.menu__list>li>a').toggleClass('toggle');
     });
+    $('.btn-menu').click(function () {
+        $('.social').slideToggle(900, 'swing');
+    });
 });
